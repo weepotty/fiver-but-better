62228ef5bc7372c215909f91bae6ed12a0119dfa
Shona Chan <mojavealoe@gmail.com>
2023-03-13 15:48:24 +0800

    changes price column of services table to integer

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,5 +2,6 @@ class Service < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: :user_id
 
   validates :title, :description, :price, :delivery_time, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 5 }
+  validates :price, numericality: { greater_than_or_equal_to: 5, less_than: 1_000_000 }, format: { with: /\A\d{1,6}(\.\d{1,2})?\z/ }
+  validates :delivery_time, numericality: { only_integer: true }, inclusion: { in: (1..90) }
 end
