7f57bb025e9a696c1ce30aad99f4fb12cb5a06f5
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-13 18:00:12 +0800

    Created new method and strong params for OffersController

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,7 +1,14 @@
 class OffersController < ApplicationController
   def new
+    @offer = Offer.new
   end
 
   def create
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:final_price, :final_delivery_time, :status, :buyer_id, :service_id)
   end
 end
