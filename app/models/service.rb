17083d2a925e1658f0929380804e2524e53aa3e8
Shona Chan <mojavealoe@gmail.com>
2023-03-13 15:19:33 +0800

    adds validations to Service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,6 @@
 class Service < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: :user_id
+
+  validates :title, :description, :price, :delivery_time, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 5 }
 end
