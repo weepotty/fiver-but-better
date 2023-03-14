31ee7962bb1894f99521d15d148f6d1ded3c160f
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-14 16:23:59 +0800

    Added comments in offers#create for easy reading

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -6,13 +6,13 @@ class OffersController < ApplicationController
 
   def create
     @offer = Offer.new(create_offer)
+    # convert input to cents
     @offer[:final_price] = create_offer[:final_price].to_i * 100
-    # for when login fucntionality is in place
-    @offer.buyer = current_user
-    # @offer.buyer = User.find(5)
 
+    @offer.buyer = current_user
     @service = Service.find(params[:service_id])
     @offer.service = @service
+
     if @offer.save
       redirect_to service_path(@service), notice: "Offer was successfully created"
     else
