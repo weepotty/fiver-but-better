0fa853abbb3bc79d5e38a03e1334d5352c6d50d9
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-14 15:52:27 +0800

    Fixed POST for offers#create
    will be running another migration after which we can remove the method on line 17 of OffersController

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,23 +1,32 @@
 class OffersController < ApplicationController
   def new
-    @service = Service.find(params[:id])
+    @service = Service.find(params[:service_id])
     @offer = Offer.new
   end
 
   def create
     @offer = Offer.new(create_offer)
-    @offer.buyer = User.find(params[:buyer_id])
-    @offer.service = Service.find(params[:service_id])
+    @offer[:final_price] = create_offer[:final_price].to_i * 100
+    # for when login fucntionality is in place
+    # @offer.buyer = current_user
+    @offer.buyer = User.find(5)
+
+    @service = Service.find(params[:service_id])
+
+    # will be running another migration afterwhich we can remove the method immediately below
+    @offer.status.capitalize!
+
+    @offer.service = @service
     if @offer.save
-      redirect_to service_path(@service)
+      redirect_to service_path(@service), notice: "Offer was successfully created"
     else
-      render :new, status: :unprocesable_entity
+      render :new, status: :unprocessable_entity, alert: "Please fill in the required field"
     end
   end
 
   private
 
   def create_offer
-    params.require(:offer).permit(:final_price, :final_delivery_time, :buyer_id, :service_id)
+    params.require(:offer).permit(:final_price, :final_delivery_time, :description, :buyer_id, :service_id)
   end
 end
