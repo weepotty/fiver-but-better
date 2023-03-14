50235695affcc5255b3fc72d9ef283ca344e4d44
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-03-14 16:20:35 +0800

    Amended offers#create with recent migrations and login funcitons

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,14 +8,10 @@ class OffersController < ApplicationController
     @offer = Offer.new(create_offer)
     @offer[:final_price] = create_offer[:final_price].to_i * 100
     # for when login fucntionality is in place
-    # @offer.buyer = current_user
-    @offer.buyer = User.find(5)
+    @offer.buyer = current_user
+    # @offer.buyer = User.find(5)
 
     @service = Service.find(params[:service_id])
-
-    # will be running another migration afterwhich we can remove the method immediately below
-    @offer.status.capitalize!
-
     @offer.service = @service
     if @offer.save
       redirect_to service_path(@service), notice: "Offer was successfully created"
