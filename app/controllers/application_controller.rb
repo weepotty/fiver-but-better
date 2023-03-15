7f60cf212d5b90a1389a37a45a5d83e73d2cde00
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-03-15 13:40:22 +0800

    Adds after sign up redirection to services_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  def after_sign_up_path_for(resource)
+    services_path
+  end
 end
