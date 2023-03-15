6ecb961f37234be019143873eea84fef7fcc9662
Shona Chan <mojavealoe@gmail.com>
2023-03-15 09:12:53 +0800

    refactors #online method in controller to an instance method on User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,9 @@
 class UsersController < ApplicationController
-
   # GET users/:id
   def show
     @user = User.find(params[:id])
     @month_created = Date::MONTHNAMES[@user.created_at.month]
-    @online = online?
     @username = @user.email.split('@').first
   end
 
-  private
-
-  def online?
-    @user.updated_at < 2.minutes.ago
-  end
 end
