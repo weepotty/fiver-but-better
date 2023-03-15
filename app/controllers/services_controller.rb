21f1cdc6009da3f201ddedebdc20373584125bfc
Shona Chan <mojavealoe@gmail.com>
2023-03-15 13:50:34 +0800

    adds route for services#create

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -10,4 +10,23 @@ class ServicesController < ApplicationController
   def new
     @service = Service.new
   end
+
+  def create
+    raise
+    @service = Service.new(service_params)
+    @service[:price] = service[:price].to_i * 100
+    @service.seller = current_user
+
+    if @service.save
+      redirect_to service_path(@service), notice: "Service was successfully created"
+    else
+      render :new, status: :unprocessable_entity, alert: "Please fill in the required fields"
+    end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :description, :category, :price, :delivery_time)
+  end
 end
