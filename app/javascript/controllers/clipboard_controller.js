adaeb45be68e6d9a2e2f7dffead0cc31613628bd
Shona Chan <mojavealoe@gmail.com>
2023-03-16 16:35:19 +0800

    creates clipboard stimulus controller and connects to share-card

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="clipboard"
+export default class extends Controller {
+  connect() {
+    console.log("hello");
+  }
+}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import ClipboardController from "./clipboard_controller"
+application.register("clipboard", ClipboardController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
