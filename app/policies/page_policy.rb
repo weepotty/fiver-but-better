f6be51a570a408e0e8848344ebe91b85dd6d6feb
Shona Chan <mojavealoe@gmail.com>
2023-03-16 11:59:27 +0800

    skips authentication on home page

---
+++ app/policies/page_policy.rb
@@ -0,0 +1,11 @@
+class PagePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def home?
+  end
+end
