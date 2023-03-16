f03f34a077de7f2d6c4b96e87b6e2dd02e9050cb
Shona Chan <mojavealoe@gmail.com>
2023-03-16 17:49:26 +0800

    adds confirmation message that link copied to clipboard

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,11 +2,17 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
+  static targets = ["confirmation"];
   connect() {
     console.log("hello");
   }
 
   copyToClipboard() {
     navigator.clipboard.writeText(window.location.href);
+    this.confirmationTarget.classList.remove("d-none");
+    const message = this.confirmationTarget;
+    setTimeout(function () {
+      message.classList.add("d-none");
+    }, 1200);
   }
 }
