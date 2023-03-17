5658509c9cf37db92243833e5c2eecfb1004e802
Shona Chan <mojavealoe@gmail.com>
2023-03-17 14:02:42 +0800

    redirects after authentication to the last page they were on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   include Pundit::Authorization
 
@@ -22,21 +23,30 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:location, :photo])
   end
 
-  def after_sign_in_path_for(resource)
-    root_path
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 
   def after_sign_out_path_for(resource)
     root_path
   end
 
-  def after_sign_up_path_for(resource)
-    root_path
+  def after_sign_up_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 
   private
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 end
