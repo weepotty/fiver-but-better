0b6eb347379c5e4f4dca831c3700ada675248313
Faris Chew <farischew@gmail.com>
2023-03-17 13:05:24 +0800

    Fixed spelling issue in review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :service
 
-  validates :commment, :rating, presence: true
+  validates :comment, :rating, presence: true
   validates :rating, inclusion: { in: 1..5 }
 end
